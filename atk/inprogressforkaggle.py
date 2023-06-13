6d41d3394645daa05d95dd972a4489b425db578d
hsouthwindj <aevin880713@gmail.com>
2023-06-14 05:16:50 +0800

    add c3d benchmark

--- atk/inprogressforkaggle.py
+++ atk/inprogressforkaggle.py
@@ -156,6 +156,123 @@ def print_output(model, img_model, vid, group_size, model_type):
     print('total frame %d, total fake frame %d', len(vid[0]), f)
     print('fake array', fa)
 
+# for benchmark robust
+import torchgeometry as tgm
+def compress_decompress(image,factor,cuda=True):
+  # :param image: tensor image
+  # :param cuda: enables cuda, must be the same parameter as the model
+  # using interpolate(input, size, scale_factor, mode, align_corners) to upsample
+  # uses either size or scale factor
+
+  image_size = list(image.size())
+  image_size = image_size[2:]
+  compressed_image = nn.functional.interpolate(image, scale_factor = factor, mode = "bilinear", align_corners = True)
+  decompressed_image = nn.functional.interpolate(compressed_image, size = image_size, mode = "bilinear", align_corners = True)
+
+  return decompressed_image
+
+
+
+def add_gaussian_noise(image,std,cuda=True):
+  # :param image: tensor image
+  # :param amount: amount of noise to be added
+  # :param cuda: enables cuda, must be the same parameter as the model
+
+
+  new_image = image + std*torch.randn_like(image)
+  new_image = torch.clamp(new_image, min=0, max=1)
+
+  return new_image
+
+
+def gaussian_blur(image,kernel_size=(11,11),sigma=(10.5, 10.5),cuda=True):
+  # smooths the given tensor with a gaussian kernel by convolving it to each channel. 
+  # It suports batched operation.
+  # :param image: tensor image
+  # :param kernel_size (Tuple[int, int]): the size of the kernel
+  # :param sigma (Tuple[float, float]): the standard deviation of the kernel
+
+  gauss = tgm.image.GaussianBlur(kernel_size, sigma)
+
+  # blur the image
+  img_blur = gauss(image)
+
+  # convert back to numpy. Turned off because I'm returning tensor
+  #image_blur = tgm.tensor_to_image(img_blur.byte())
+
+  return img_blur
+
+
+def translate_image(image, shift_x = 20, shift_y = 20, cuda=True):
+
+  image_size = list(image.size())
+  image_size = image_size[2:]
+
+  h, w = image_size[0], image_size[1]  
+
+
+  #because height indexed from zero
+  points_src = torch.FloatTensor([[
+    [0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1],
+  ]])
+
+  # the destination points are the image vertexes
+  points_dst = torch.FloatTensor([[
+    [0 + shift_x, 0 + shift_y], [w-1+shift_x, 0 + shift_y], [w-1+shift_x, h-1 + shift_y], [0 + shift_x, h-1 + shift_y],]])
+
+  if cuda:
+    points_src = points_src.cuda()
+    points_dst = points_dst.cuda()
+  # compute perspective transform
+  print(type(points_src), points_src)
+  M = cv2.getPerspectiveTransform(points_src.cpu().detach().numpy(), points_dst.cpu().detach().numpy()) # try to use library in cv
+  M = torch.from_numpy(M).float().to(device='cuda')
+  # warp the original image by the found transform
+  img_warp = tgm.warp_perspective(image, M, dsize=(h, w))
+
+  return img_warp
+
+def _get_transforms(apply_transforms = {"gauss_noise", "gauss_blur", "translation", "resize"}):
+        cuda = True
+        transform_list = [
+            lambda x: x,
+        ]
+
+        if "gauss_noise" in apply_transforms:
+            transform_list += [
+                lambda x: add_gaussian_noise(x, 0.01, cuda = cuda),
+            ]
+
+        if "gauss_blur" in apply_transforms:
+            transform_list += [
+                lambda x: gaussian_blur(x, kernel_size = (5, 5), sigma=(5., 5.), cuda = cuda),
+                lambda x: gaussian_blur(x, kernel_size = (5, 5), sigma=(10., 10.), cuda = cuda),
+                lambda x: gaussian_blur(x, kernel_size = (7, 7), sigma=(5., 5.), cuda = cuda),
+                lambda x: gaussian_blur(x, kernel_size = (7, 7), sigma=(10., 10.), cuda = cuda),
+            ]
+
+        if "translation" in apply_transforms:
+            transform_list += [
+                lambda x: translate_image(x, 10, 10, cuda = cuda),
+                lambda x: translate_image(x, 10, -10, cuda = cuda),
+                lambda x: translate_image(x, -10, 10, cuda = cuda),
+                lambda x: translate_image(x, -10, -10, cuda = cuda),
+                lambda x: translate_image(x, 20, 20, cuda = cuda),
+                lambda x: translate_image(x, 20, -20, cuda = cuda),
+                lambda x: translate_image(x, -20, 10, cuda = cuda),
+                lambda x: translate_image(x, -20, -20, cuda = cuda),
+            ]
+
+        if "resize" in apply_transforms:
+            transform_list += [
+                lambda x: compress_decompress(x, 0.1, cuda = cuda),
+                lambda x: compress_decompress(x, 0.2, cuda = cuda),
+                lambda x: compress_decompress(x, 0.3, cuda = cuda),
+            ]
+
+        return transform_list
+
+
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
 group_size = 7 # default 7
