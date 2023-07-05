fde5eb8e7f394f49ce0c670c5a8903c90e2a493c
hsouthwindj <aevin880713@gmail.com>
2023-07-06 01:47:40 +0800

    change print lines

--- atk/imgatkforkaggle.py
+++ atk/imgatkforkaggle.py
@@ -918,6 +918,11 @@ args = parser.parse_args()
 
 group_size = args.group_size
 
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, module="torch")
+warnings.filterwarnings("ignore", category=SourceChangeWarning, module="torch.serialization")
+
 for arg, value in sorted(vars(args).items()):
     logging.info("Argument %s: %r", arg, value)
 
