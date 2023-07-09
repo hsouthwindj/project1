1edd15f0dfb91e69b53e46cd4c2f9690fd68e155
hsouthwindj <aevin880713@gmail.com>
2023-07-09 14:49:52 +0800

    mod for simba

--- atk/utils/simba.py
+++ atk/utils/simba.py
@@ -114,7 +114,6 @@ class SimBA:
                 remaining = preds.ne(labels_batch)
             else:
                 remaining = preds.eq(labels_batch)
-            print('remaining', remaining)
             remaining = torch.Tensor([True]).repeat(len(labels_batch)).bool()
             # check if all images are misclassified and stop early
             if remaining.sum() == 0 and k > 100:
