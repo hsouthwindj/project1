323c2d7adabd2167d7895c790b4e5401f77e6ab2
hsouthwindj <aevin880713@gmail.com>
2023-07-09 20:10:52 +0800

    mod for simba

--- atk/otheratkforkaggle.py
+++ atk/otheratkforkaggle.py
@@ -527,7 +527,7 @@ class TrackSequencesClassifier(object):
         simba = SimBA(img_model, 'a', si)
         # out = simba.simba_single(rsf(track_sequences)[0], torch.Tensor([1]).long().cuda())
         # print(out)
-        out = simba.simba_batch(rsf(track_sequences), torch.Tensor([1]).repeat(len(track_sequences)).long().cuda(), 5000, 192, 12, 25/255, pixel_attack = True)
+        out = simba.simba_batch(rsf(track_sequences), torch.Tensor([1]).repeat(len(track_sequences)).long().cuda(), 5000, 192, 12, 10/255, pixel_attack = True)
         advs = out[0]
         l2s = out[4]
         
